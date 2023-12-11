aa28e8a16e0025d6d9e541349dfce77fb0718d52
Reid Wilbur <reid.wilbur@gmail.com>
2023-12-11 10:39:22 -0600

    cleanup

--- src/main/java/com/wilb0t/aoc/Day11.java
+++ src/main/java/com/wilb0t/aoc/Day11.java
@@ -3,12 +3,7 @@ package com.wilb0t.aoc;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 class Day11 {
 
@@ -36,27 +31,27 @@ class Day11 {
         .filter(c -> IntStream.range(0, image.vals.length).allMatch(r -> image.vals[r][c] == '.'))
         .boxed().toList();
 
-    var pathSum = 0L;
-    for (var idx = 0; idx < image.galaxies.size(); idx++) {
-      for (var other = idx + 1; other < image.galaxies.size(); other++) {
-        var galPos = image.galaxies.get(idx);
-        var ogalPos = image.galaxies.get(other);
+    return IntStream.range(0, image.galaxies.size()).mapToLong(
+        idx -> {
+          var galPos = image.galaxies.get(idx);
+          return IntStream.range(idx + 1, image.galaxies.size()).mapToLong(
+              oidx -> {
+                var ogalPos = image.galaxies.get(oidx);
 
-        long hdist = Math.abs(galPos.c - ogalPos.c);
-        long hexp = expCols.stream()
-            .filter(c -> c >= Math.min(galPos.c, ogalPos.c) && c <= Math.max(galPos.c, ogalPos.c()))
-            .count();
-        hdist += hexp * (expAmt - 1);
+                long hdist = Math.abs(galPos.c - ogalPos.c);
+                long hexp = expCols.stream()
+                    .filter(c -> c >= Math.min(galPos.c, ogalPos.c) && c <= Math.max(galPos.c, ogalPos.c))
+                    .count();
+                hdist += hexp * (expAmt - 1);
 
-        long vdist = Math.abs(galPos.r - ogalPos.r);
-        long vexp = expRows.stream()
-            .filter(r -> r >= Math.min(galPos.r, ogalPos.r) && r <= Math.max(galPos.r, ogalPos.r()))
-            .count();
-        vdist += vexp * (expAmt - 1);
+                long vdist = Math.abs(galPos.r - ogalPos.r);
+                long vexp = expRows.stream()
+                    .filter(r -> r >= Math.min(galPos.r, ogalPos.r) && r <= Math.max(galPos.r, ogalPos.r))
+                    .count();
+                vdist += vexp * (expAmt - 1);
 
-        pathSum += vdist + hdist;
-      }
-    }
-    return pathSum;
+                return vdist + hdist;
+              }).sum();
+        }).sum();
   }
 }
