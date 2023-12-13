75d0b5ed2684870d7cd28447336ad30be9e779fb
Reid Wilbur <reid.wilbur@gmail.com>
2023-12-12 19:19:17 -0600

    wip

--- src/main/java/com/wilb0t/aoc/Day12.java
+++ src/main/java/com/wilb0t/aoc/Day12.java
@@ -3,6 +3,7 @@ package com.wilb0t.aoc;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Day12 {
@@ -15,50 +16,141 @@ class Day12 {
       return new SpringRecord(status, grpSizes);
     }
 
-    static boolean isValid(String pattern, List<Integer> grpSizes) {
-      var brokenGrps = new ArrayList<Integer>();
-      char last = '.';
-      for (var idx = 0; idx < pattern.length(); idx++) {
-        char cur = pattern.charAt(idx);
-        if (cur  == '#') {
-          if (last == '#' && !brokenGrps.isEmpty()) {
-            brokenGrps.set(brokenGrps.size() - 1, brokenGrps.getLast() + 1);
-          } else {
-            brokenGrps.add(1);
-          }
-        }
-        last = cur;
-      }
-      return brokenGrps.equals(grpSizes);
+    public SpringRecord expand() {
+      var expstat = IntStream.range(0, 5).mapToObj(i -> status).collect(Collectors.joining("?"));
+      var grpSizes = IntStream.range(0, 5).boxed().flatMap(i -> grpSizes().stream()).toList();
+      return new SpringRecord(expstat, grpSizes);
     }
 
-    public int countValidPatterns() {
-      var qcount = status.chars().filter(c -> c == '?').count();
+//    static boolean isValid(String pattern, List<Integer> grpSizes) {
+//      var brokenGrps = new ArrayList<Integer>();
+//      char last = '.';
+//      for (var idx = 0; idx < pattern.length(); idx++) {
+//        char cur = pattern.charAt(idx);
+//        if (cur  == '#') {
+//          if (last == '#' && !brokenGrps.isEmpty()) {
+//            brokenGrps.set(brokenGrps.size() - 1, brokenGrps.getLast() + 1);
+//          } else {
+//            brokenGrps.add(1);
+//          }
+//        }
+//        last = cur;
+//      }
+//      return brokenGrps.equals(grpSizes);
+//    }
 
-      if (qcount == 0) {
-        return 1;
-      }
+//    public int countValidPatterns() {
+//      var qcount = status.chars().filter(c -> c == '?').count();
+//
+//      if (qcount == 0) {
+//        return 1;
+//      }
+//
+//      var validcount = 0;
+//      var combs = (1 << qcount);
+//      for (var itr = 0; itr < combs; itr++) {
+//        var sb = new StringBuilder(status);
+//        var qidx = 0;
+//        for (var idx = 0; idx < sb.length(); idx++) {
+//          if (sb.charAt(idx) == '?') {
+//            char val = ((itr & (1L << qidx)) == 0) ? '.' : '#';
+//            sb.setCharAt(idx, val);
+//            qidx += 1;
+//          }
+//        }
+//        validcount += isValid(sb.toString(), grpSizes) ? 1 : 0;
+//      }
+//
+//      return validcount;
+//    }
+  }
 
-      var validcount = 0;
-      var combs = (1 << qcount);
-      for (var itr = 0; itr < combs; itr++) {
-        var sb = new StringBuilder(status);
-        var qidx = 0;
-        for (var idx = 0; idx < sb.length(); idx++) {
-          if (sb.charAt(idx) == '?') {
-            char val = ((itr & (1L << qidx)) == 0) ? '.' : '#';
-            sb.setCharAt(idx, val);
-            qidx += 1;
-          }
-        }
-        validcount += isValid(sb.toString(), grpSizes) ? 1 : 0;
-      }
-
-      return validcount;
+  static boolean reject(String candidate, List<Integer> grpSizes) {
+    var lastq = candidate.indexOf('?');
+    if (lastq == -1) {
+      return false;
     }
+    var end = candidate.substring(0, lastq).lastIndexOf('.');
+    if (end == -1) {
+      return false;
+    }
+    var csub = candidate.substring(0, end);
+
+    var brokenGrps = new ArrayList<Integer>();
+    char last = '.';
+    for (var idx = 0; idx < csub.length(); idx++) {
+      char cur = csub.charAt(idx);
+      if (cur  == '#') {
+        if (last == '#' && !brokenGrps.isEmpty()) {
+          brokenGrps.set(brokenGrps.size() - 1, brokenGrps.getLast() + 1);
+        } else {
+          brokenGrps.add(1);
+        }
+      }
+      last = cur;
+    }
+
+    var reject = false;
+    for (var idx = 0; idx < Math.min(brokenGrps.size(), grpSizes.size()); idx++) {
+      if (!brokenGrps.get(idx).equals(grpSizes.get(idx))) {
+        reject = true;
+      }
+    }
+    return reject;
+  }
+
+  static boolean accept(String candidate, List<Integer> grpSizes) {
+    var brokenGrps = new ArrayList<Integer>();
+    char last = '.';
+    for (var idx = 0; idx < candidate.length(); idx++) {
+      char cur = candidate.charAt(idx);
+      if (cur  == '#') {
+        if (last == '#' && !brokenGrps.isEmpty()) {
+          brokenGrps.set(brokenGrps.size() - 1, brokenGrps.getLast() + 1);
+        } else {
+          brokenGrps.add(1);
+        }
+      }
+      last = cur;
+    }
+    return brokenGrps.equals(grpSizes);
+  }
+
+  static String replaceFirst(char c, char r, String s) {
+    var idx = s.indexOf(c);
+    if (idx == -1) {
+      return s;
+    }
+    var sb = new StringBuilder(s);
+    sb.setCharAt(idx, r);
+    return sb.toString();
+  }
+
+  public static int getValidPatterns(String candidate, List<Integer> grpSizes) {
+    if (!candidate.contains("?")) {
+      return accept(candidate, grpSizes) ? 1 : 0;
+    }
+
+    if (reject(candidate, grpSizes)) {
+      return 0;
+    }
+
+    var left = replaceFirst('?', '.', candidate);
+    var right = replaceFirst('?', '#', candidate);
+
+    var validcount = 0;
+    validcount += getValidPatterns(left, grpSizes);
+    validcount += getValidPatterns(right, grpSizes);
+
+    return validcount;
   }
 
   public static int getValidPatterns(List<SpringRecord> records) {
-    return records.stream().mapToInt(SpringRecord::countValidPatterns).sum();
+    //return records.stream().mapToInt(SpringRecord::countValidPatterns).sum();
+    return records.stream().mapToInt(sr -> getValidPatterns(sr.status, sr.grpSizes)).sum();
+  }
+
+  public static int getValidPatternsExpanded(List<SpringRecord> records) {
+    return records.stream().map(SpringRecord::expand).mapToInt(sr -> getValidPatterns(sr.status, sr.grpSizes)).sum();
   }
 }
--- src/test/java/com/wilb0t/aoc/Day12Test.java
+++ src/test/java/com/wilb0t/aoc/Day12Test.java
@@ -12,16 +12,28 @@ public class Day12Test {
   private static final List<SpringRecord> TEST_INPUT = Input.TEST.load(SpringRecord::from);
   private static final List<SpringRecord> PUZZLE_INPUT = Input.PUZZLE.load(SpringRecord::from);
 
+//  @Test
+//  void testCountValidPatterns() {
+//    var record = SpringRecord.from("#.#.### 1,1,3");
+//    assertThat(record.countValidPatterns(), is(1));
+//
+//    record = SpringRecord.from(".??..??...?##. 1,1,3");
+//    assertThat(record.countValidPatterns(), is(4));
+//
+//    record = SpringRecord.from("?###???????? 3,2,1");
+//    assertThat(record.countValidPatterns(), is(10));
+//  }
+
   @Test
-  void testCountValidPatterns() {
+  void testGetValidPatterns() {
     var record = SpringRecord.from("#.#.### 1,1,3");
-    assertThat(record.countValidPatterns(), is(1));
+    assertThat(Day12.getValidPatterns(record.status(), record.grpSizes()), is(1));
 
     record = SpringRecord.from(".??..??...?##. 1,1,3");
-    assertThat(record.countValidPatterns(), is(4));
+    assertThat(Day12.getValidPatterns(record.status(), record.grpSizes()), is(4));
 
     record = SpringRecord.from("?###???????? 3,2,1");
-    assertThat(record.countValidPatterns(), is(10));
+    assertThat(Day12.getValidPatterns(record.status(), record.grpSizes()), is(10));
   }
 
   @Test
@@ -33,4 +45,15 @@ public class Day12Test {
   void testCountValidPatterns_puzzleInput() {
     assertThat(Day12.getValidPatterns(PUZZLE_INPUT), is(6488));
   }
+
+  @Test
+  void testCountValidPatternsExpanded_testInput() {
+    assertThat(Day12.getValidPatternsExpanded(TEST_INPUT), is(525152));
+  }
+
+  @Test
+  void testCountValidPatternsExpanded_puzzleInput() {
+    assertThat(Day12.getValidPatternsExpanded(PUZZLE_INPUT), is(525152));
+  }
+
 }
