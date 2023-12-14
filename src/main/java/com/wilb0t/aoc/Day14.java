77f047605b2ba2c966ad4ab406f7a9c638973b8e
Reid Wilbur <reid.wilbur@gmail.com>
2023-12-14 16:31:36 -0600

    Day 14 part 2

--- src/main/java/com/wilb0t/aoc/Day14.java
+++ src/main/java/com/wilb0t/aoc/Day14.java
@@ -1,7 +1,10 @@
 package com.wilb0t.aoc;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.IntStream;
@@ -22,6 +25,15 @@ class Day14 {
   }
 
   public record Dish(Element[][] state) {
+
+    public Dish copyOf() {
+      Element[][] copyState = new Element[state.length][];
+      for (var row = 0; row < state.length; row++) {
+        copyState[row] = Arrays.copyOf(state[row], state[row].length);
+      }
+      return new Dish(copyState);
+    }
+
     public static Dish from(List<String> lines) {
       Element[][] state = new Element[lines.size()][];
       for (var row = 0; row < lines.size(); row++) {
@@ -30,23 +42,130 @@ class Day14 {
       return new Dish(state);
     }
 
-    int getNorthLoad(int col) {
-      var weight = state.length;
+    int getLoad(int col) {
       var totalWeight = 0;
       for (var row = 0; row < state.length; row++) {
         var element = state[row][col];
         if (element == Element.ROUND_ROCK) {
-          totalWeight += weight;
-          weight -= 1;
-        } else if (element == Element.CUBE_ROCK) {
-          weight = state.length - (row + 1);
+          totalWeight += state.length - row;
         }
       }
       return totalWeight;
     }
 
-    public int getNorthLoad() {
-      return IntStream.range(0, state[0].length).map(this::getNorthLoad).sum();
+    public int getLoad() {
+      return IntStream.range(0, state[0].length).map(this::getLoad).sum();
     }
+
+
+    public void cycleNorth() {
+      boolean updated = true;
+      int width = state[0].length;
+      while (updated) {
+        updated = false;
+        for (var row = 1; row < state.length; row++) {
+          for (var col = 0; col < width; col++) {
+            var el = state[row][col];
+            if (el == Element.ROUND_ROCK && state[row - 1][col] == Element.EMPTY) {
+              state[row - 1][col] = Element.ROUND_ROCK;
+              state[row][col] = Element.EMPTY;
+              updated = true;
+            }
+          }
+        }
+      }
+    }
+
+    public void cycleWest() {
+      boolean updated = true;
+      int width = state[0].length;
+      while (updated) {
+        updated = false;
+        for (var col = 1; col < width; col++) {
+          for (var row = 0; row < state.length; row++) {
+            var el = state[row][col];
+            if (el == Element.ROUND_ROCK && state[row][col - 1] == Element.EMPTY) {
+              state[row][col - 1] = Element.ROUND_ROCK;
+              state[row][col] = Element.EMPTY;
+              updated = true;
+            }
+          }
+        }
+      }
+    }
+
+    public void cycleSouth() {
+      boolean updated = true;
+      int width = state[0].length;
+      while (updated) {
+        updated = false;
+        for (var row = state.length - 2; row >= 0; row--) {
+          for (var col = 0; col < width; col++) {
+            var el = state[row][col];
+            if (el == Element.ROUND_ROCK && state[row + 1][col] == Element.EMPTY) {
+              state[row + 1][col] = Element.ROUND_ROCK;
+              state[row][col] = Element.EMPTY;
+              updated = true;
+            }
+          }
+        }
+      }
+    }
+
+    public void cycleEast() {
+      boolean updated = true;
+      int width = state[0].length;
+      while (updated) {
+        updated = false;
+        for (var col = width - 2; col >= 0; col--) {
+          for (var row = 0; row < state.length; row++) {
+            var el = state[row][col];
+            if (el == Element.ROUND_ROCK && state[row][col + 1] == Element.EMPTY) {
+              state[row][col + 1] = Element.ROUND_ROCK;
+              state[row][col] = Element.EMPTY;
+              updated = true;
+            }
+          }
+        }
+      }
+    }
+
+    public void cycle() {
+      cycleNorth();
+      cycleWest();
+      cycleSouth();
+      cycleEast();
+    }
+  }
+
+  public record CycleInfo(int startIdx, int size) {}
+
+  public static CycleInfo getCycle(List<Integer> vals) {
+    var wsize = vals.size() / 2;
+    var ofs = 0;
+    for (; wsize > 3; wsize--) {
+      for (ofs = 0; ofs < (vals.size() - (wsize * 2)); ofs++) {
+        if (vals.subList(ofs, ofs + wsize).equals(vals.subList(ofs + wsize, ofs + (wsize * 2)))) {
+          return new CycleInfo(ofs, wsize);
+        }
+      }
+    }
+    throw new RuntimeException("no cycle found");
+  }
+
+  public static int spinCycle(Dish dish, int cycles) {
+    var workingDish = dish.copyOf();
+
+    var sample = 250;
+    var loads = new ArrayList<Integer>(sample);
+    for (var idx = 0; idx < sample; idx++) {
+      workingDish.cycle();
+      loads.add(workingDish.getLoad());
+    }
+
+    var cycleInfo = getCycle(loads);
+    cycles -= cycleInfo.startIdx + 1;
+    var mod = cycles % cycleInfo.size;
+    return loads.get(cycleInfo.startIdx + mod);
   }
 }
--- src/test/java/com/wilb0t/aoc/Day14Test.java
+++ src/test/java/com/wilb0t/aoc/Day14Test.java
@@ -3,7 +3,10 @@ package com.wilb0t.aoc;
 import static com.wilb0t.aoc.Day14.Dish;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class Day14Test {
@@ -13,18 +16,113 @@ public class Day14Test {
 
   @Test
   void testGetNorthLoad() {
-    assertThat(TEST_INPUT.getNorthLoad(0), is(34));
-    assertThat(TEST_INPUT.getNorthLoad(1), is(27));
-    assertThat(TEST_INPUT.getNorthLoad(2), is(17));
+    var ti = TEST_INPUT.copyOf();
+    ti.cycleNorth();
+    assertThat(ti.getLoad(0), is(34));
+    assertThat(ti.getLoad(1), is(27));
+    assertThat(ti.getLoad(2), is(17));
   }
 
   @Test
   void testGetNorthLoad_testInput() {
-    assertThat(TEST_INPUT.getNorthLoad(), is(136));
+    var ti = TEST_INPUT.copyOf();
+    ti.cycleNorth();
+    assertThat(ti.getLoad(), is(136));
   }
 
   @Test
   void testGetNorthLoad_puzzleInput() {
-    assertThat(PUZZLE_INPUT.getNorthLoad(), is(108641));
+    var pi = PUZZLE_INPUT.copyOf();
+    pi.cycleNorth();
+    assertThat(pi.getLoad(), is(108641));
+  }
+
+  @Test
+  void testCycleNorth() {
+    var lines =
+        List.of(
+            "OOOO.#.O..",
+            "OO..#....#",
+            "OO..O##..O",
+            "O..#.OO...",
+            "........#.",
+            "..#....#.#",
+            "..O..#.O.O",
+            "..O.......",
+            "#....###..",
+            "#....#....");
+    var expDish = Dish.from(lines);
+    var ti = TEST_INPUT.copyOf();
+    ti.cycleNorth();
+    for (var row = 0; row < ti.state().length; row++) {
+      assertArrayEquals(ti.state()[row], expDish.state()[row]);
+    }
+  }
+
+  @Test
+  void testCycle() {
+    var lines =
+        List.of(
+            ".....#....",
+            "....#...O#",
+            "...OO##...",
+            ".OO#......",
+            ".....OOO#.",
+            ".O#...O#.#",
+            "....O#....",
+            "......OOOO",
+            "#...O###..",
+            "#..OO#...."
+        );
+    var expDish = Dish.from(lines);
+    var ti = TEST_INPUT.copyOf();
+    ti.cycle();
+    for (var row = 0; row < ti.state().length; row++) {
+      assertArrayEquals(ti.state()[row], expDish.state()[row]);
+    }
+
+    lines = List.of(
+        ".....#....",
+        "....#...O#",
+        ".....##...",
+        "..O#......",
+        ".....OOO#.",
+        ".O#...O#.#",
+        "....O#...O",
+        ".......OOO",
+        "#..OO###..",
+        "#.OOO#...O");
+    expDish = Dish.from(lines);
+    ti.cycle();
+    for (var row = 0; row < ti.state().length; row++) {
+      assertArrayEquals(ti.state()[row], expDish.state()[row]);
+    }
+
+    lines = List.of(
+        ".....#....",
+        "....#...O#",
+        ".....##...",
+        "..O#......",
+        ".....OOO#.",
+        ".O#...O#.#",
+        "....O#...O",
+        ".......OOO",
+        "#...O###.O",
+        "#.OOO#...O");
+    expDish = Dish.from(lines);
+    ti.cycle();
+    for (var row = 0; row < ti.state().length; row++) {
+      assertArrayEquals(ti.state()[row], expDish.state()[row]);
+    }
+  }
+
+  //@Test
+  void testSpinCycle_testInput() {
+    assertThat(Day14.spinCycle(TEST_INPUT, 1_000_000_000), is(64));
+  }
+
+  @Test
+  void testSpinCycle_puzzleInput() {
+    assertThat(Day14.spinCycle(PUZZLE_INPUT, 1_000_000_000), is(84328));
   }
 }
